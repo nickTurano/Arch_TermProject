2f2eb5910c1552e53666cf90c3ded8aac1524a62
nickTurano <nickt803@gmail.com>
2017-03-01 17:54:47 -0500

    Created ALU entity with 2 source 32 bit vectors

--- ALU.vhd
+++ ALU.vhd
@@ -1,1 +1,11 @@
-
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+entity ALU is
+  port(
+    src1, src2: in STD_LOGIC_VECTOR(31 down to 0);
+    result: out STD_LOGIC_VECTOR(31 down to 0);
+    zero: out bit;
+  );
+  
