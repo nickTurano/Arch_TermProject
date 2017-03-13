2015547abb628ecadaf16dc81358905ea67c8ea8
nickTurano <nickt803@gmail.com>
2017-03-12 20:49:24 -0400

    Added structural mapping for lower half of diagram

--- MIPS.vhd
+++ MIPS.vhd
@@ -23,7 +23,7 @@ architecture structure of MIPS is
     );
   end component;
 
-  component 2x1mux is
+  component 32Bit_2x1mux is
     port(
       x, y: in STD_LOGIC_VECTOR(31 downto 0);
       sel: in bit;
@@ -31,11 +31,18 @@ architecture structure of MIPS is
     );
   end component;
 
+  component 5Bit_2x1mux is
+    port(
+      x, y: in STD_LOGIC_VECTOR(5 downto 0);
+      sel: in bit;
+      z: out STD_LOGIC_VECTOR(5 downto 0)
+    );
+  end component;
+
   component Control is
     port(
       inst: in STD_LOGIC_VECTOR(5 downto 0);
-      RegDst, Jump, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite: out bit;
-      ALUOp: out STD_LOGIC_VECTOR(1 downto 0)
+      RegDst, Jump, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, ALUOP1, ALUOP2: out bit;
     );
   end component;
 
@@ -45,6 +52,7 @@ architecture structure of MIPS is
       MemWrite, MemRead,ck: in STD_LOGIC;
       read_data: out STD_LOGIC_VECTOR (31 downto 0)
     );
+  end component;
 
   component ALU is
     port(
@@ -59,6 +67,38 @@ architecture structure of MIPS is
     port(
       read_reg_1, read_reg_2, write_reg: in STD_LOGIC_VECTOR(5 downto 0);
       write_data: in STD_LOGIC_VECTOR(31 downto 0);
+      RegWrite: in bit;
       read_data_1, read_data_2: out STD_LOGIC_VECTOR(31 downto 0)
     );
   end component;
+
+component Sign_Entender is
+  port(
+    x: in STD_LOGIC_VECTOR(15 downto 0);
+    y: out STD_LOGIC_VECTOR(31 downto 0)
+  );
+end component;
+
+component ALU_Control is
+  port(
+    inst: in STD_LOGIC_VECTOR(5 downto 0);
+    OP1, OP2: in bit;
+    ALU_Inst: out STD_LOGIC_VECTOR(3 downto 0)
+  );
+end component;
+
+  signal pc, instruction, Write_data, ReadData1, ReadData2, inst_extd, ALU_Input, ALU_Result, memory_data, memory_result: STD_LOGIC_VECTOR(31 downto 0);
+  signal RegDst, Jump, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, ALUOP1, ALUOP2, zero: out bit;
+  signal Write_Register STD_LOGIC_VECTOR(5 downto 0);
+  signal ALUInst: STD_LOGIC_VECTOR(3 downto 0);
+  begin
+    Instruction_Memory: Inst_Mem port map(address=>pc, instruction=>instruction);
+    Control_Unit: Control port map(inst=>instruction(31 downto 26), RegDst=>RegDst, Jump=>Jump, MemRead=>MemRead, MemtoReg=>MemtoReg, MemWrite=?MemWrite, ALUSrc=>ALUSrc, RegWrite=>RegWrite, ALUOP1=>ALUOP1, ALUOP2=>ALUOP2);
+    5BMux1: 5Bit_2x1mux port map(x=>instruction(20 downto 16), y=>instruction(15 downto 11), sel=>RegDst, z=>Write_Register);
+    Registers: Registers port map(read_reg_1=>instruction(25 downto 21), read_reg_2=>instruction(20 downto 16), write_reg=>Write_Register, write_data=>Write_data, RegWrite=>RegWrite, read_data_1=>ReadData1, read_data_2=>ReadData2);
+    S1: Sign_Entender port map(x=>instruction(15 downto 0), y=>inst_extd);
+    ALUControl: ALU_Control port map(inst=>instruction(5 downto 0), OP1=>ALUOP1, OP2=>ALUOP2, ALU_Inst=>ALUInst);
+    32BMux1: 32Bit_2x1mux port map(x=>ReadData2, y=>inst_extd, sel=>ALUSrc, z=>ALU_Input);
+    ALU: ALU port map(src1=>ReadData1, src2=>Alu_Input, OP=>ALUInst, result=>ALU_Result, zero=>zero);
+    Data_Memory: data_mem port map(address=>ALU_Result, write_date=>ReadData2, MemWrite=>MemWrite, MemRead=>MemRead, ck=>clk, read_data=>memory_data);
+    32BMux2: 32Bit_2x1mux port map(x=>ALU_Result, y=>memory_data, sel=>MemtoReg, z=>memory_result);
